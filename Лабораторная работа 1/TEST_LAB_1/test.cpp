28d19a010c543638f9a29ae890de9a64c7bbe12b
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-09-14 14:22:41 +0300

    Add GenerateReportFile test case

--- Лабораторная работа 1/TEST_LAB_1/test.cpp
+++ Лабораторная работа 1/TEST_LAB_1/test.cpp
@@ -1,6 +1,57 @@
 #include "pch.h"
+#include <fstream>
+#include <string>
+#include "../Reporter/employee.h"
+using std::string;
+using std::ifstream;
+using std::ofstream;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+int generate_report(const string& bin_file, const string& rep_file, double pay) {
+    ifstream fin(bin_file, std::ios::binary);
+    if (!fin.is_open()) {
+        return 2;
+    }
+    ofstream fout(rep_file);
+    if (!fout) {
+        return 3;
+    }
+
+    fout << "Report for \"" << bin_file << "\" file\n";
+    fout << "----------------------------------------------------\n";
+    fout << "Num\tName\tHours\tSalary\n";
+
+    employee emp;
+    while (fin.read(reinterpret_cast<char*>(&emp), sizeof(emp))) {
+        double salary = emp.hours * pay;
+        fout << emp.num << "\t" << emp.name << "\t"
+            << emp.hours << "\t" << std::fixed << std::setprecision(2) << salary << "\n";
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
+
+TEST(ReporterTest, GeneratesReportFile) {
+    const char* test_bin = "test_employee.bin";
+    const char* test_report = "test_report.txt";
+    employee emp = {1, "John", 40.0};
+    {
+        ofstream fout(test_bin, std::ios::binary);
+        fout.write(reinterpret_cast<const char*>(&emp), sizeof(emp));
+    }
+
+    int result = generate_report(test_bin, test_report, 10.0);
+
+    EXPECT_EQ(result, 0);
+
+    ifstream fin(test_report);
+    string content, line;
+    while (std::getline(fin, line)) {
+        content += line + "\n";
+    }
+    EXPECT_NE(content.find("John"), string::npos);
+    EXPECT_NE(content.find("400.00"), string::npos);
+
+    remove(test_bin);
+    remove(test_report);
 }
