81ff0ea9baf3fd687ca97c7c4a50a563879ac813
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-09-29 22:12:48 +0300

    Rewritten using ArrayHandler and ThreadHandler classes

--- Lab 2 c++20/main.cpp
+++ Lab 2 c++20/main.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
-#include <memory>
-#include <windows.h>
 #include <vector>
 #include "ArrayHandler.h"
+#include "ThreadHandler.h"
 using std::cout;
 using std::cin;
 using std::vector;
 
-
-
 int main() {
-    Holder holder = { ArrayHandler::getArray(), -1, -1, 0 };
-    HANDLE h_min_max = CreateThread(NULL, 0, ArrayHandler::findMinMax, &holder, 0, NULL);
-    if (h_min_max == NULL) { return static_cast<int>(GetLastError()); }
-    WaitForSingleObject(h_min_max, INFINITE);
-    CloseHandle(h_min_max);
+    Holder holder (ArrayHandler::getArray(), -1, -1, 0 );
 
+    ThreadHandler::launchFindMinMaxThread(
+        ArrayHandler::findMinMax, holder);
     cout<<"\nMAX: " << holder.array[holder.min_index] <<"\nMIN: " << holder.array[holder.max_index];
     cout<<"\nMAX index: " << holder.max_index <<"\nMIN index: " << holder.min_index <<"\n";
 
-    HANDLE h_average = CreateThread(NULL, 0, ArrayHandler::findAverage, &holder, 0, NULL);
-    if (h_average == NULL) { return static_cast<int>(GetLastError()); }
-    WaitForSingleObject(h_average, INFINITE);
-    CloseHandle(h_average);
+    ThreadHandler::launchFindAverageThread(
+        ArrayHandler::findAverage, holder);
     cout<<"\nAverage: " << holder.average <<"\n";
 
-    holder.array[holder.min_index] = holder.average;
-    holder.array[holder.max_index] = holder.average;
-    cout<<'\n';
-    for (const int i : holder.array) {
-        cout << i << " ";
-    }
+    ArrayHandler::setMinMaxToAverage(holder);
+    for (const int i : holder.array) { cout << i << " "; }
     return 0;
 }
