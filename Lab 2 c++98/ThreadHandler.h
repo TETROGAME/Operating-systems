5280ae5cd14d8252565af6fc37f931d88755abb4
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-09-29 21:13:00 +0300

    Moved thread logic from main to dedicated class

---
+++ Lab 2 c++98/ThreadHandler.h
@@ -0,0 +1,12 @@
+#ifndef THREADHANDLER_H
+#define THREADHANDLER_H
+
+#include "ArrayHandler.h"
+
+class ThreadHandler {
+public:
+    static void findMinMaxThread(Holder& holder);
+    static void findAverageThread(Holder& holder);
+};
+
+#endif //THREADHANDLER_H
