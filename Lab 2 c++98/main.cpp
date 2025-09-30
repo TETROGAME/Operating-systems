53a98bbe66d0e8fdcff45b83a139ca02136fe7ba
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-09-30 10:45:15 +0300

    Removed redundant qualifiers

--- Lab 2 c++98/main.cpp
+++ Lab 2 c++98/main.cpp
@@ -8,16 +8,16 @@ using std::vector;
 using namespace ThreadHandler;
 using namespace ArrayHandler;
 int main() {
-    Holder holder (ArrayHandler::getArray(), -1, -1, 0 );
+    Holder holder (getArray(), -1, -1, 0 );
 
-    ThreadHandler::findMinMaxThread(holder);
+    findMinMaxThread(holder);
     cout<<"\nMAX: " << holder.array[holder.min_index] <<"\nMIN: " << holder.array[holder.max_index];
     cout<<"\nMAX index: " << holder.max_index <<"\nMIN index: " << holder.min_index <<"\n";
 
-    ThreadHandler::findAverageThread(holder);
+    findAverageThread(holder);
     cout<<"\nAverage: " << holder.average <<"\n";
 
-    ArrayHandler::setMinMaxToAverage(holder);
+    setMinMaxToAverage(holder);
     for (int i = 0; i < holder.array.size(); i++) { cout << holder.array[i] << ' '; }
     return 0;
 }
