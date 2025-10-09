bc0e1df83b3da917697e226609424ae3b26d3c29
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-09 08:43:39 +0300

    Initial commit

---
+++ Lab 3/ThreadHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 09.10.2025.
+//
+
+#include "ThreadHandler.h"
---
+++ Lab 3/ThreadHandler.h
@@ -0,0 +1,13 @@
+//
+// Created by User on 09.10.2025.
+//
+
+#ifndef LAB_3_THREADHANDLER_H
+#define LAB_3_THREADHANDLER_H
+
+
+class ThreadHandler {
+};
+
+
+#endif //LAB_3_THREADHANDLER_H
