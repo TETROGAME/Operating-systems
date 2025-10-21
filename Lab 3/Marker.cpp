bbef2a37cb906e573a07e6614552ba1e4a96370e
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-21 12:33:09 +0300

    Add getters and finished run function

--- Lab 3/Marker.cpp
+++ Lab 3/Marker.cpp
@@ -1,36 +1,76 @@
 #include "Marker.h"
 #include <iostream>
 #include <chrono>
-Marker::Marker(int id, Array &array): id_(id), array_(array), blocked_(false), shouldFinish_(false) {}
+
+Marker::Marker(int id, Array& array, std::condition_variable& start_cv, std::mutex& start_mtx, bool& ready)
+    : id_(id), array_(array), start_cv_(start_cv), start_mtx_(start_mtx), ready_to_start_(ready) {}
 
 void Marker::run() {
+    thread_id_ = std::this_thread::get_id();
+    {
+        std::unique_lock<std::mutex> lock(start_mtx_);
+        start_cv_.wait(lock, [this]{ return ready_to_start_; });
+    }
+
     srand(id_);
     int counter = 0;
-    while (true) {
+
+    while (!finished_) {
         int index = rand() % array_.size();
+
+        std::lock_guard<std::mutex> array_lock(array_.get_mutex());
         if (array_[index] == 0) {
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
             array_[index] = id_;
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
             counter++;
-        }
-        else {
-            std::cout << "\nThread number : " << id_ << '\n';
-            std::cout << "Marked " << counter << " elements\n";
-            std::cout << "Failed to mark element at " << index << " position\n";
+        } else {
+            std::cout << "\nThread number: " << id_ << '\n'
+                      << "Marked " << counter << " elements\n"
+                      << "Failed to mark element at " << index << " position\n";
+
             blocked_ = true;
 
-            std::unique_lock<std::mutex> unique_lock(mtx_);
-            cv_.wait(unique_lock, [this]{ return shouldFinish_ || !blocked_; });
+            // 3.4.3 Wait for main's signal
+            std::unique_lock<std::mutex> lock(mtx_);
+            cv_.wait(lock, [this]{ return !blocked_ || finished_; });
+        }
+        if (finished_) {
+            break;
+        }
+    }
 
-            if (shouldFinish_) {
-                for (int& elem : array_) {
-                    if (elem == id_) elem = 0;
-                }
-                blocked_ = false;
-                std::cout << "Thread " << id_ << " finished\n";
-                break;
+    std::cout << "Thread " << id_ << " is cleaning up...\n";
+    {
+        std::lock_guard<std::mutex> array_lock(array_.get_mutex());
+        for (int& elem : array_) {
+            if (elem == id_) {
+                elem = 0;
             }
         }
     }
+    finished_ = true;
+    std::cout << "Thread " << id_ << " finished.\n";
 }
+
+void Marker::unblock() {
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        blocked_ = false;
+    }
+    cv_.notify_one();
+}
+
+void Marker::finish() {
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        finished_ = true;
+        blocked_ = false;
+    }
+    cv_.notify_one();
+}
+
+int Marker::get_id() const { return id_; }
+bool Marker::is_blocked() const { return blocked_; }
+bool Marker::is_finished() const { return finished_; }
+std::thread::id Marker::get_thread_native_handle() const { return thread_id_; }
--- Lab 3/Marker.h
+++ Lab 3/Marker.h
@@ -1,24 +1,37 @@
 #ifndef LAB_3_MARKER_H
 #define LAB_3_MARKER_H
 #include <condition_variable>
+#include <atomic>
+#include <thread>
 #include "Array.h"
-
 
 class Marker {
     int id_;
-    Array array_;
+    Array& array_;
     static constexpr int sleepTime = 5;
 
-public:
-    std::atomic<bool> blocked_;
-    std::atomic<bool> shouldFinish_;
+    std::atomic<bool> blocked_{false};
+    std::atomic<bool> finished_{false};
     std::condition_variable cv_;
     std::mutex mtx_;
 
-    Marker(int id, Array& array);
+    std::condition_variable& start_cv_;
+    std::mutex& start_mtx_;
+    bool& ready_to_start_;
+
+    std::thread::id thread_id_;
+
+public:
+    Marker(int id, Array& array, std::condition_variable& start_cv, std::mutex& start_mtx, bool& ready);
 
     void run();
-};
+    void unblock();
+    void finish();
 
+    int get_id() const;
+    bool is_blocked() const;
+    bool is_finished() const;
+    std::thread::id get_thread_native_handle() const;
+};
 
 #endif //LAB_3_MARKER_H
