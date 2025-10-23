27c5dc578f08f0325825e87ef8931f446b3fdd97
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-23 15:13:16 +0300

    Add explicit template definition

--- Colloquium/delete_duplicates.cpp
+++ Colloquium/delete_duplicates.cpp
@@ -1,11 +1,21 @@
 #include "include/delete_duplicates.h"
+#include <unordered_set>
+#include <vector>
+
+using std::vector;
+
 template <typename T>
 vector<T> delete_duplicates(const vector<T>& in) {
     vector<T> out;
     out.reserve(in.size());
     std::unordered_set<T> seen;
     for (T v : in) {
-        if (!seen.contains(v)) { seen.insert(v); out.push_back(v); }
+        if (!seen.contains(v)) {
+            seen.insert(v);
+            out.push_back(v);
+        }
     }
     return out;
 }
+
+template vector<int> delete_duplicates<int>(const vector<int>&);
