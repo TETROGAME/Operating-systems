031a36df9d00f95eb68644cc15a11e6fe841a81a
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-23 15:12:05 +0300

    Moved remaining realizations to .cpp file

--- Colloquium/include/linked_list.h
+++ Colloquium/include/linked_list.h
@@ -8,14 +8,14 @@ template <typename T>
 struct Node {
     T v;
     std::shared_ptr<Node<T>> next;
-    explicit Node(T val): v(std::move(val)), next(nullptr) {}
+    explicit Node(T val);
 };
 
 template <typename T>
 class LinkedList {
     std::shared_ptr<Node<T>> head;
 public:
-    LinkedList() = default;
+    LinkedList();
     LinkedList(std::initializer_list<T> init);
     void append(const T& value);
     std::vector<T> to_vector() const;
@@ -23,5 +23,4 @@ public:
 private:
     std::shared_ptr<Node<T>> reverse_impl(std::shared_ptr<Node<T>> node);
 };
-
 #endif //COLLOQUIUM_LINKED_LIST_H
