7769c7f770998863003db58361e93da9d0f1742d
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-23 08:17:50 +0300

    Add move constructor and assignment operator

--- Lab 3/Array.cpp
+++ Lab 3/Array.cpp
@@ -1,8 +1,12 @@
 #include "Array.h"
 
+#include <mutex>
+#include <utility>
+Array::Array() : data_(nullptr), size_(0) {}
+
 Array::Array(int size) {
     if (size <= 0) {
-        throw std::invalid_argument("Size must be positive integer");
+        throw std::invalid_argument("Size must be a positive integer");
     }
     this->data_ = std::make_shared<int[]>(size);
     this->size_ = size;
@@ -10,22 +14,38 @@ Array::Array(int size) {
 
 Array::~Array() = default;
 
+Array::Array(Array&& other) noexcept
+    : data_(std::move(other.data_)), size_(other.size_) {
+    other.size_ = 0;
+}
+Array& Array::operator=(Array&& other) noexcept {
+    if (this != &other) {
+        std::scoped_lock lock(mtx_, other.mtx_);
+
+        data_ = std::move(other.data_);
+        size_ = other.size_;
+        other.size_ = 0;
+    }
+    return *this;
+}
+
 int Array::size() const { return size_; }
 
 int& Array::operator[](int index) {
-    if (index >= size_) throw std::out_of_range("Index out of range");
+    if (index < 0 || index >= size_) throw std::out_of_range("Index out of range");
     return data_[index];
 }
 
-const int & Array::operator[](int index) const {
-    if (index >= size_) throw std::out_of_range("Index out of range");
+const int& Array::operator[](int index) const {
+    if (index < 0 || index >= size_) throw std::out_of_range("Index out of range");
     return data_[index];
 }
 
-int* Array::data() { return data_.get(); }
-const int* Array::data() const { return data_.get(); }
+std::mutex& Array::get_mutex() {
+    return mtx_;
+}
 
-int * Array::begin() { return data_.get(); }
-int * Array::end() { return data_.get() + size_; }
-const int * Array::begin() const { return data_.get(); }
-const int * Array::end() const { return data_.get() + size_; }
+int* Array::begin() { return data_.get(); }
+int* Array::end() { return data_.get() + size_; }
+const int* Array::begin() const { return data_.get(); }
+const int* Array::end() const { return data_.get() + size_; }
--- Lab 3/Array.h
+++ Lab 3/Array.h
@@ -1,27 +1,33 @@
 #ifndef LAB_3_ARRAY_H
 #define LAB_3_ARRAY_H
-
 #include <memory>
 
 class Array {
     std::shared_ptr<int[]> data_;
     int size_;
+    std::mutex mtx_;
+
 public:
-    Array(int size);
+    Array();
+    explicit Array(int size);
     ~Array();
+
+    Array(const Array&) = delete;
+    Array& operator=(const Array&) = delete;
+
+    Array(Array&& other) noexcept;
+    Array& operator=(Array&& other) noexcept;
 
     int size() const;
     int& operator[](int index);
     const int& operator[](int index) const;
 
-    int* data();
-    const int* data() const;
+    std::mutex& get_mutex();
 
     int* begin();
     int* end();
     const int* begin() const;
     const int* end() const;
 };
-
 
 #endif //LAB_3_ARRAY_H
