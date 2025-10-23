ffaf1cda3a3420f70b157daa6bdaf46dd8cb11d6
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-23 08:19:02 +0300

    Add basic functionality

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
-#include <vector>
+#include "Array.h"
+#include "ThreadHandler.h"
+
 using std::cout;
 using std::cin;
-using std::vector;
+
 int main() {
+    int size = 0;
+    cout << "Input array size: ";
+    cin >> size;
+    Array array;
+    try {
+        array = Array(size);
+    } catch (const std::invalid_argument& exception) {
+        cout << "Error: " << exception.what() << '\n';
+        return 1;
+    }
+
+    for (int& elem : array) {
+        elem = 0;
+    }
+    cout << "Initial array: ";
+    for (const int& elem : array) {
+        cout << elem << ' ';
+    }
+    cout << "\n";
+
+    int thread_count = 0;
+    cout << "Input desired thread number: ";
+    cin >> thread_count;
+    if (thread_count <= 0) {
+        cout << "Error: Thread count must be a positive integer.\n";
+        return 1;
+    }
+
+    ThreadHandler handler(thread_count, array);
+    handler.run();
 
     return 0;
 }
