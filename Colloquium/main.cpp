d34f69f7635e30e5f85c0419df4b328306c11cf5
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-10-25 17:21:36 +0300

    Add manual input numbers/arrays input

--- Colloquium/main.cpp
+++ Colloquium/main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include "include/factorial.h"
 #include "include/delete_duplicates.h"
 #include "include/linked_list.h"
-
+namespace solution {
+    using std::cout;
+    using std::cin;
+    using std::vector;
+}
+using namespace solution;
 int main() {
     // Task 1: first n factorials (stops before overflow)
-    auto facts = generate_factorials(25);
-    std::cout << "factorials (" << facts.size() << "):\n";
+    cout << "Input number of factorials to calculate: ";
+    int factorial_number;
+    cin >> factorial_number;
+    auto facts = generate_factorials(factorial_number);
+    cout << "factorials (" << facts.size() << "):\n";
     for (size_t i = 0; i < facts.size(); ++i) {
-        std::cout << (i + 1) << "! = " << facts[i] << '\n';
+        cout << (i + 1) << "! = " << facts[i] << '\n';
     }
 
     // Task 2: remove duplicates while preserving order
-    std::vector<int> v = {1,2,3,2,4,1};
+    cout << "Input array size: ";
+    int size;
+    cin >> size;
+    cout << "Input " << size << "array elements: ";
+    vector<int> v(size);
+    for (int i = 0; i < size; i++) {
+        cin >> v[i];
+    }
     auto u = delete_duplicates(v);
-    std::cout << "\nunique ints:";
+    std::cout << "Unique numbers:";
     for (auto x : u) std::cout << ' ' << x;
     std::cout << '\n';
 
 
     // Task 3: reverse singly linked list using recursion
-    LinkedList<int> list = {1,2,3,4,5};
+    cout << "Input LinkedList size: ";
+    cin >> size;
+    cout << "Input " << size << " elements for LinkedList";
+    LinkedList<int> list;
+    int temp;
+    for (int i = 0; i < size; i++) {
+        cin >> temp;
+        list.append(temp);
+    }
     std::cout << "\nlist before:";
     for (auto x : list.to_vector()) std::cout << ' ' << x;
     std::cout << '\n';
