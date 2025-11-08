ee20423de3ab0f9e389dc36678270ed653e15c47
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-11-08 12:45:16 +0300

    Initial commit

---
+++ Lab_4/Receiver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 08.11.2025.
+//
+
+#include "Receiver.h"
---
+++ Lab_4/Receiver.h
@@ -0,0 +1,10 @@
+#ifndef LAB_4_RECEIVER_H
+#define LAB_4_RECEIVER_H
+
+
+class Receiver {
+
+};
+
+
+#endif //LAB_4_RECEIVER_H
