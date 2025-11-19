5865d0d8394697b99e5d3161a5650d2fb81d6644
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-11-19 22:35:13 +0300

    Add solution namespace

--- Lab_4_C++98/src/Sender.cpp
+++ Lab_4_C++98/src/Sender.cpp
@@ -1,52 +1,54 @@
 #include "../include/SharedQueue.h"
 #include <iostream>
+#include <solution_namespace.h>
 #include <string>
+using namespace solution;
 
 int main(int argc, char* argv[]) {
-    std::cout << "=== Sender ===\n";
+    cout << "=== Sender ===\n";
     if (argc < 2) {
-        std::cout << "Usage: sender.exe <fileName>\n";
+        cout << "Usage: sender.exe <fileName>\n";
         return 1;
     }
-    std::string fileName = argv[1];
+    string fileName = argv[1];
 
     SharedQueue queue;
     if (!queue.OpenAsSender(fileName)) {
-        std::cerr << "[Sender] Can't open queue.\n";
+        cerr << "[Sender] Can't open queue.\n";
         return 1;
     }
 
     if (!queue.SignalSenderReady()) {
-        std::cerr << "[Sender] Can't sent ready signal.\n";
+        cerr << "[Sender] Can't sent ready signal.\n";
         return 1;
     }
-    std::cout << "[Sender] Ready. Commands: s - send, q - quit.\n";
+    cout << "[Sender] Ready. Commands: s - send, q - quit.\n";
 
     while (true) {
-        std::cout << "[Sender] Command (s/q): ";
-        std::string cmdLine;
-        if (!std::getline(std::cin, cmdLine)) break;
+        cout << "[Sender] Command (s/q): ";
+        string cmdLine;
+        if (!getline(cin, cmdLine)) break;
         if (cmdLine == "s") {
             if (queue.IsShuttingDown()) {
-                std::cout << "[Sender] Receiver finishes. Quitting.\n";
+                cout << "[Sender] Receiver finishes. Quitting.\n";
                 break;
             }
-            std::cout << "Input message (<20 symbols): ";
-            std::string msg;
-            if (!std::getline(std::cin, msg)) break;
+            cout << "Input message (<20 symbols): ";
+            string msg;
+            if (!getline(cin, msg)) break;
             if (msg.size() >= MAX_MESSAGE_LEN) {
-                std::cout << "[Sender] Message too long.\n";
+                cout << "[Sender] Message too long.\n";
                 continue;
             }
             if (!queue.PushMessage(msg, true)) {
-                std::cout << "[Sender] Error.\n";
+                cout << "[Sender] Error.\n";
                 if (queue.IsShuttingDown()) break;
             }
         } else if (cmdLine == "q") {
-            std::cout << "[Sender] Quitting.\n";
+            cout << "[Sender] Quitting.\n";
             break;
         } else {
-            std::cout << "Unknown commands.\n";
+            cout << "Unknown commands.\n";
         }
     }
     return 0;
