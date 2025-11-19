bb6069383f8ba088670c0b978f07742ddc2c37ec
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-11-19 22:18:59 +0300

    Initial commit

---
+++ Lab_4_C++98/include/solution_namespace.h
@@ -0,0 +1,13 @@
+#ifndef LAB_4C_98_NAMESPACE_SOLUTION_H
+#define LAB_4C_98_NAMESPACE_SOLUTION_H
+namespace solution {
+    using std::cin;
+    using std::cout;
+    using std::err;
+
+    using std::string;
+    using std::getline;
+
+    using std::vector;
+}
+#endif //LAB_4C_98_NAMESPACE_SOLUTION_H
