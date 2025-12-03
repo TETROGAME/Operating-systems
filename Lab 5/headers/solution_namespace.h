abd56860cdc0e243112d298b61181c367b38b334
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-12-03 16:00:51 +0300

    Initial commit

---
+++ Lab 5/headers/solution_namespace.h
@@ -0,0 +1,25 @@
+#ifndef LAB_5_SOLUTION_NAMESPACE_H
+#define LAB_5_SOLUTION_NAMESPACE_H
+
+#include <cstddef>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+
+namespace sln {
+    using std::size_t;
+    using std::memcpy;
+    using std::string;
+    using std::cout;
+    using std::cerr;
+    using std::endl;
+    using std::ifstream;
+    using std::ofstream;
+    using std::fstream;
+    using std::remove;
+}
+
+#endif //LAB_5_SOLUTION_NAMESPACE_H
