83ef43b4c6f587309fb498443b04818fd0200366
Nikita Timoshenko <minecrosoft231231@gmail.com>
2025-12-04 13:54:23 +0300

    Add Employee I/O logic, client process launching logic

--- Lab 5/src/server.cpp
+++ Lab 5/src/server.cpp
@@ -62,5 +62,76 @@ void ProcessClient(HANDLE hPipe, FileHandler& fileHelper) {
 }
 
 int main() {
+    string filename;
+    int count;
 
+    cout << "Enter filename: ";
+    cin >> filename;
+
+    cout << "Enter number of employees: ";
+    cin >> count;
+
+    vector<Employee> employees;
+    for(int i = 0; i < count; ++i) {
+        int id; string name; double h;
+        cout << "Employee " << (i+1) << " ID: "; cin >> id;
+        cout << "Employee " << (i+1) << " Name: "; cin >> name;
+        cout << "Employee " << (i+1) << " Hours: "; cin >> h;
+        employees.push_back(Employee(id, name, h));
+    }
+
+    FileHandler fh(filename);
+    fh.CreateFileWithData(employees);
+    fh.ReadAllAndPrint();
+
+    cout << "Enter number of client processes to start: ";
+    int clients;
+    cin >> clients;
+
+    for(int i = 0; i < clients; ++i) {
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si)); si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+
+        char cmdLine[] = "client.exe";
+        if (!CreateProcess(NULL, cmdLine, NULL, NULL, FALSE,
+                           CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+            cout << "Failed to create client process." << endl;
+        } else {
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+        }
+    }
+
+    cout << "Server listening on pipe..." << endl;
+
+    for(int i=0; i < clients; ++i) {
+        HANDLE hPipe = CreateNamedPipe(
+            PIPE_NAME,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            4096, 4096, 0, NULL
+        );
+
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            cout << "Failed to create pipe." << endl;
+            return 1;
+        }
+
+        if (ConnectNamedPipe(hPipe, NULL) != FALSE) {
+            cout << "Client connected." << endl;
+            ProcessClient(hPipe, fh);
+        } else {
+             CloseHandle(hPipe);
+        }
+    }
+
+    cout << "All clients finished." << endl;
+    fh.ReadAllAndPrint();
+
+    cout << "Press any key to exit server...";
+    char c; cin >> c;
+    return 0;
 }
