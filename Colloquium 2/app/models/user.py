b7d53435576351e78288bce576aa25b664ee29a8
Nikita Timoshenko <minecrosoft231231@gmai.com>
2025-12-27 19:27:47 +0300

    Add users table to db

---
+++ Colloquium 2/app/models/user.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    disabled: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    created_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=True)
+    updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=True)
