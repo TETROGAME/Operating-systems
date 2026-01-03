b36b0f625dda35356706023d63ba4bb7ca497513
Nikita Timoshenko <minecrosoft231231@gmai.com>
2026-01-03 15:29:51 +0300

    Add basic health endpoint to validate hostname

--- Colloquium 2/app/core/factory.py
+++ Colloquium 2/app/core/factory.py
@@ -9,6 +9,7 @@ from app.core.logging_config import configure_logging
 from app.core.metrics import add_metrics
 from app.routers.auth import router as auth_router
 from app.routers.tasks import router as tasks_router
+from app.routers.health import router as health_router
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +32,7 @@ def create_app() -> FastAPI:
 
     app.include_router(auth_router)
     app.include_router(tasks_router)
+    app.include_router(health_router)
 
     logger.info("Application started")
     return app
